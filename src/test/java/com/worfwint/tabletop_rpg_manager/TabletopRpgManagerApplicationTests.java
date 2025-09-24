2c4d302ac5adef10be564e4b161f2dbb72347b4c
michael <khudoleev04@mail.ru>
2025-09-24 23:15:28 +0500

    Initial commit. Spring has been initialized.

---
+++ src/test/java/com/worfwint/tabletop_rpg_manager/TabletopRpgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worfwint.tabletop_rpg_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopRpgManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
